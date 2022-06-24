75ab0962d07144b77c9356d7201fefd55e6ae3fa
ISAAC <isaacaea10@gmail.com>
2022-06-24 15:35:06 -0500

    otro intento

---
+++ js/codigojs.js
@@ -0,0 +1,9 @@
+const navMenu = document.querySelector("#navMenu");
+const botonMenu = document.querySelector("#botonMenu");
+
+botonMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+})
+
+
+
